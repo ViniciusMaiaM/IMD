dcb48a731107ea72e6fd359dc779d529c0a2ba61
Vinicius <viniciusmaiamarinho1@gmail.com>
2022-10-10 20:16:21 -0300

    add correção de chamada de js

--- ex1-s5.js
+++
@@ -1,7 +0,0 @@
-function executarExercicio() {
-    var data = new Date();
-    var texto = data.getUTCHours();
-    var min = data.getUTCMinutes();
-    var sec = data.getUTCSeconds();
-    document.getElementById('resultado').innerHTML = texto+':'+min+':'+sec;
-}
--- ex3-s4.js
+++
@@ -1,9 +0,0 @@
-function executarExercicio() {
-    var a = Number(document.getElementById('a').value);
-    var b = Number(document.getElementById('b').value);
-    var c = Number(document.getElementById('c').value);
-    var d = Number(document.getElementById('d').value);
-    var menor = Math.min(a,b,c,d); 
-    var maior = Math.max(a,b,c,d);
-    document.getElementById('resultado').innerHTML = "O maior valor é: " + maior + "</br>" + "O menor valor é: " + menor;
-}
