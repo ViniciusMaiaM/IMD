c8e3e667caed7040187369f51bd4963f3a8b26fb
Vinicius <viniciusmaiamarinho1@gmail.com>
2022-11-14 20:08:09 -0300

    add modificação na classe cellphone

---
+++ Poo/aulas/aula6/app/js/cellphone.js
@@ -0,0 +1,72 @@
+"use strict";
+// O modificador protected define que classes filhas/derivadas também tem acesso
+// ao atributo ou método
+// Ele chamará o construtor da superclasse para que essa possa realizar 
+// as suas inicializações antes mesmo que todas as suas classes filhas
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Cellphone = void 0;
+var Cellphone = /** @class */ (function () {
+    function Cellphone(battery, chip) {
+        this._battery = battery;
+        this._chip = chip;
+        if (battery == undefined || battery < 0) {
+            this._battery = 100;
+        }
+        if (chip == undefined || chip <= 0) {
+            this._chip = 1;
+        }
+    }
+    Cellphone.prototype.power = function (action) {
+        if (action) {
+            console.log("The cellphone is on!");
+        }
+        else {
+            console.log("The cellphone is of!");
+        }
+    };
+    Cellphone.prototype.call = function (queue) {
+        if (queue > 0) {
+            console.log("You have ".concat(queue, " call(s) in the queue!"));
+        }
+        else {
+            console.log("You don't have any call in queue!");
+        }
+    };
+    Object.defineProperty(Cellphone.prototype, "battery", {
+        get: function () {
+            return this._battery;
+        },
+        set: function (battery) {
+            if (battery < 0) {
+                this._battery = 100;
+            }
+            else {
+                this._battery = battery;
+            }
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Cellphone.prototype, "chip", {
+        get: function () {
+            return this._chip;
+        },
+        set: function (chip) {
+            if (chip <= 0) {
+                this._chip = 1;
+            }
+            else {
+                this._chip = chip;
+            }
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Cellphone.prototype.detail = function () {
+        console.log("\n        Battery: ".concat(this._battery, "\n        Chip(s): ").concat(this._chip));
+    };
+    return Cellphone;
+}());
+exports.Cellphone = Cellphone;
+var c = new Cellphone();
+c.detail();
---
+++ Poo/aulas/aula6/app/ts/cellphone.ts
@@ -0,0 +1,76 @@
+// O modificador protected define que classes filhas/derivadas também tem acesso
+// ao atributo ou método
+// Ele chamará o construtor da superclasse para que essa possa realizar 
+// as suas inicializações antes mesmo que todas as suas classes filhas
+
+export class Cellphone{
+    protected _battery: number;
+    protected _chip: number;
+
+    public constructor(battery?: number, chip?: number){
+        this._battery = battery;
+        this._chip = chip;
+
+        if(battery == undefined || battery < 0){
+            this._battery = 100;
+        }
+
+        if(chip == undefined || chip <= 0){
+            this._chip = 1;
+        }
+    }
+
+    public power(action:boolean){
+        if(action){
+            console.log("The cellphone is on!");
+        }
+        
+        else{
+            console.log("The cellphone is of!");
+        }
+    }
+
+    public call(queue?: number){
+        if(queue > 0){
+            console.log(`You have ${queue} call(s) in the queue!`);
+        }
+        else{
+            console.log("You don't have any call in queue!");
+        }
+    }
+
+    public get battery():number{
+        return this._battery
+    }
+
+    public set battery(battery:number){
+        if(battery < 0){
+            this._battery = 100;
+        }
+        else{
+            this._battery = battery;
+        }
+    }
+
+    public get chip():number{
+        return this._chip
+    }
+
+    public set chip(chip:number){
+        if(chip <= 0){
+            this._chip = 1;
+        }
+        else{
+            this._chip = chip;
+        }
+    }
+
+    public detail(){
+        console.log(`
+        Battery: ${this._battery}
+        Chip(s): ${this._chip}`)
+    }
+}
+
+let c = new Cellphone();
+c.detail();
